675e483dfdde22a5250e1b032913354e4152fba3
Ranjithprabhu K <ranjithprabhu.k@gmail.com>
2017-11-09 16:30:53 +0530

    Add product done
    Add product is done
    Product list added to a json
    Dynamic data defined in the html

--- src/app/item-selector/item-selector.component.ts
+++ src/app/item-selector/item-selector.component.ts
@@ -7,5 +7,111 @@ import { ItemSelectorService } from './item-selector.service';
 })
 export class ItemSelectorComponent {
 
- 
+    constructor(private settleService: ItemSelectorService) { }
+
+    public itemList = [
+        {
+            "categoryId": 1,
+            "categoryName": "All",
+            "products": [
+                {
+                    "productId": 11,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 12,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 13,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 14,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 15,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 16,
+                    "productName": "Product 1"
+                }
+            ]
+        },
+        {
+            "categoryId": 1,
+            "categoryName": "All",
+            "products": [
+                {
+                    "productId": 11,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 12,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 13,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 14,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 15,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 16,
+                    "productName": "Product 1"
+                }
+            ]
+        },
+        {
+            "categoryId": 1,
+            "categoryName": "All",
+            "products": [
+                {
+                    "productId": 11,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 12,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 13,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 14,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 15,
+                    "productName": "Product 1"
+                },
+                {
+                    "productId": 16,
+                    "productName": "Product 1"
+                }
+            ]
+        }
+    ];
+
+    public selectedCategory = this.itemList[0];
+    public selectedProducts = [];
+
+
+
+    public addProduct(product) : void {
+        if(product){
+            this.selectedProducts.push(product);
+        }
+    }
+
+
 }
